a0a4c6dfd3c71e2aa794e7a34468bbd18f57aaae
mimalefdal <mimalefdal@yahoo.com>
2021-03-19 19:34:43 +0330

    Try to extend getFeatureActivated by using AppContext (Unsuccessful)

--- resources/js/utils/getFeatureActivated.js
+++ resources/js/utils/getFeatureActivated.js
@@ -1,11 +1,24 @@
-const _getFeatureActivated = feature => {
-    const features = JSON.parse(sessionStorage.getItem("features")).map(
-        item => item.activation && item.slug
-    );
-    // console.log("_getFeatureActivated", features);
-    if (features.indexOf(feature) != -1) return true;
-    // console.log(feature, "Forbidden");
-    return false;
+import React, { useContext } from "react";
+import { getMatchIndexOf } from ".";
+import AppContext from "../context/appContext";
+
+// function _getFeatureActivated(feature, features = null) {
+//     console.log("_getFeatureActivated", feature);
+//     if (!features) features = useContext(AppContext).features;
+//     console.log("_getFeatureActivated", features);
+
+//     // let _activated = getMatchIndexOf(features, "slug", feature) != -1;
+//     // console.log("_getFeatureActivated", feature, _activated);
+//     return true;
+// }
+
+const _getFeatureActivated = (feature, features = null) => {
+    if (!features)
+        features = JSON.parse(sessionStorage.getItem("features")).map(
+            item => item.activation && item.slug
+        );
+
+    return features.indexOf(feature) != -1;
 };
 
 export default _getFeatureActivated;
